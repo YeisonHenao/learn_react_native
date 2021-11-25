b8ff3d0aec539b222bae554fd43e47f320b00d69
Sr. Yeison Alexander Henao Osorio <yhenaoo@coopebombas.local>
2021-11-25 17:10:27 -0500

    Funcionalidad de detalles del producto seleccionado

--- Screens/DetailStore.js
+++ Screens/DetailStore.js
@@ -1,22 +1,44 @@
 import React, { useEffect, useState } from 'react';
-import {View , Text , StyleSheet , Image} from 'react-native';
+import {View , Text , StyleSheet , Image , ScrollView , SafeAreaView} from 'react-native';
 
 const DetailStore = ({route}) => {
 
-  const data = route.params;
+  const data = route.params.product;
 
   const style = StyleSheet.create({
-    productImage:{
-      height:50,
-      width:50,
-      marginLeft:60
+    image:{
+      height:'50%',
+      width:'80%',
+      borderRadius:10,
+      alignSelf:'center',
+      marginTop:5,
+      marginBottom:5
+    },
+    headerTitle:{
+      textAlign:'center',
+      fontWeight:'bold',
+      fontSize:20
+    },
+    descriptionText:{
+      fontSize:16,
+      margin:8
+    },
+    priceText:{
+      textAlign:'center',
+      fontSize:16,
+      color:'red'
     }
   })
   
   return(
     <View>
-      <Text>Detail Screen</Text>
-      <Text>titulo - {JSON.stringify(data.title)}</Text>
+          <Text style={style.headerTitle}>{JSON.stringify(data.title).slice(1,-1)}</Text>
+          <Image 
+            source={{uri:data.image}}
+            style={style.image}
+          />
+          <Text style={style.descriptionText}>{JSON.stringify(data.description).slice(1,-1)}</Text>
+          <Text style={style.priceText}>{JSON.stringify(data.price)}</Text>
     </View>
   );
 }
