a7d7112288e1127f30784c17d30deb7198112d26
Yeison Henao <yeisonhenaoo@gmail.com>
2022-12-06 23:14:05 -0500

    Solución novedad de SDK y librerias para ejecutar proyecto

--- Components/BadgeShoppingCart.js
+++ Components/BadgeShoppingCart.js
@@ -1,12 +1,13 @@
 import React from "react";
+import { View } from "react-native";
 import Feather from "react-native-vector-icons/Feather";
 
 
 function BadgeShoppingCart() {
   return (
-    <div>
+    <View>
       <Feather name="shopping-cart" size={8} color={"#fff"} />
-    </div>
+    </View>
   );
 }
 
